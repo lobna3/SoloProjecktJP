e14ae46b9183cc84d4044f326adebcdf4601c7db
lobna3 <yousfi55lobna@gmail.com>
2024-06-19 11:50:17 +0200

    create db

--- BackEnd/database/index.js
+++ BackEnd/database/index.js
@@ -12,3 +12,35 @@ connection.connect().then(() => {
 }).catch((error) => {
     console.log(error)
 })
+
+module.exports = {
+    getAll: function () {
+        const sql = "select * from `job`"
+        return connection.query(sql)
+    },
+    add: function (name, descreption, category, city,email) {
+        const sql = "insert into `product` (name,descreption,category,city,email) values(?,?,?,?,?)"
+        return connection.query(sql, [name, descreption, category, city, email])
+    },
+    getById: function (id) {
+        const sql = "select * from `job` where id = ?"
+        return connection.query(sql, id)
+    },
+    getByName: function (name) {
+        const sql = "select * from `job` where name = ?"
+        return connection.query(sql, name)
+    },
+    getByCategory: function (category) {
+        const sql = "select * from `job` where category = ?"
+        return connection.query(sql, category)
+    },
+    deleteP: function (id) {
+        const sql = "delete from `product` where id = ?"
+        return connection.query(sql, id)
+    },
+    update: function (name, descreption, price, category, imageUrl,id) {
+        const sql = "update `product` set name=?,descreption=?,price=?,category=?,imageUrl=? where id=?"
+        return connection.query(sql, [name, descreption, price, category, imageUrl,id])
+    },
+
+}
